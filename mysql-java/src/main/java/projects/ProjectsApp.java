67b4cd3eea70191a0b74e2083c9410086cc6ffca
christophergeorgeelliott <115366782+christophergeorgeelliott@users.noreply.github.com>
2024-02-03 17:13:34 -0800

    Add files via upload

---
+++ mysql-java/src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,15 @@
+package projects;
+
+import projects.dao.DbConnection;
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DbConnection.getConnection();
+
+	}
+
+}
